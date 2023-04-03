0be664619f0b80c5dad3610d0cd5bf5a59353598
Júlia Suriani <89102778+JuliaSuriani@users.noreply.github.com>
2023-04-03 20:14:52 +0000

    nada

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    
+    Scanner scan = new Scanner(System.in);
+    
+    System.out.println("");
+    
   }
 }
