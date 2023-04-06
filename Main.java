cdc94b403dc77dc4f943c4bd5fba716333a422f9
Júlia Suriani <89102778+JuliaSuriani@users.noreply.github.com>
2023-04-06 20:45:23 +0000

    nada bb

--- Main.java
+++ Main.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
 
 class Main {
+  static final int teste = 0;
+
   public static void main(String[] args) {
-    
+    string nome, cargo, cpf = " ";
+    double salarioBase, salarioFinal = 0;
     Scanner scan = new Scanner(System.in);
-    
+
     System.out.println("");
-    
+
   }
 }
