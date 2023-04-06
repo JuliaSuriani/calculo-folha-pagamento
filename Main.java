bc4793b1a00cc7aa56f17727dab2ce4f80eb2fa5
Júlia Suriani <89102778+JuliaSuriani@users.noreply.github.com>
2023-04-06 21:03:38 +0000

    adição de variáveis e entrada de valores

--- Main.java
+++ Main.java
@@ -1,14 +1,24 @@
 import java.util.Scanner;
 
 class Main {
-  static final int teste = 0;
+  static final int vt = 0.06; // vale transporte desconto de 6%
+  static final int va = 0.2; // vale alimentação desconto de 20%
 
   public static void main(String[] args) {
     string nome, cargo, cpf = " ";
-    double salarioBase, salarioFinal = 0;
+    float salarioBase, salarioFinal = 0;
+    float inss, fgts, irrf = 0;
     Scanner scan = new Scanner(System.in);
 
-    System.out.println("");
+    System.out.println("Insira o nome do funcinário:");
+    nome = console.next();
+
+    System.out.println("Insira o CPF do funcinário:");
+    cpf = console.next();
+
+    System.out.println("Insira o salário base do funcinário:");
+    salarioBase = console.nextFloat();
+
 
   }
 }
